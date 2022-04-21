3311e4df6bbb13fdac4c82c871e85dc48e47fac0
Nelson Florisbal <11463555+nflorisbal@users.noreply.github.com>
2022-04-21 07:52:55 -0300

    add global.styles file

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from './router';
 
+import './index.css';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
