37cf56923e1fa567d5bcd71ec3741d202163af4e
Nelson Florisbal <11463555+nflorisbal@users.noreply.github.com>
2022-04-21 08:44:59 -0300

    add theme file and login style

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,6 +1,25 @@
+import * as Yup from 'yup';
+import { Formik } from 'formik';
+
+const FORM_INITIAL_VALUES = {
+  email: '',
+  password: '',
+};
+
+const loginSchema = Yup.object().shape({
+  email: Yup.string().required('Campo obrigatório.'),
+  password: Yup.string().required('Campo obrigatório.'),
+});
+
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
-export default Login
+    <Formik
+      initialValues={FORM_INITIAL_VALUES}
+      validationSchema={loginSchema}
+      onSubmit={() => console.log('logou')}
+    >
+      
+    </Formik>
+  );
+};
+export default Login;
---
+++ src/theme.tsx
@@ -0,0 +1,13 @@
+export const Theme = {
+  color: {
+    primaryLight: '#F392C4',
+    primaryPure: '#E62589',
+    primaryDark: '#921154',
+    secondaryLight: '#BA95DE',
+    secondaryPure: '#752CBC',
+    secondaryDark: '#471B73',
+    highlightLight: '#A9E0F3',
+    highlightPure: '#53C1E8',
+    highlightDark: '#156C8B',
+  },
+};
