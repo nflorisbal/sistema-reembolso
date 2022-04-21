d0760de77149a77bc55dab36cee5f88352a57b94
Nelson Florisbal <11463555+nflorisbal@users.noreply.github.com>
2022-04-21 07:34:46 -0300

    add page signup

--- src/router.tsx
+++ src/router.tsx
@@ -2,17 +2,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import NotFound from './pages/notfound/NotFound';
+import SignUp from './pages/signup/SignUp';
 
-const Routers = () => {
+const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="*" element={<NotFound />} />
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
 };
 
-export default Routers;
+export default Router;
