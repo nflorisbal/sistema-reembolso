e889e1c0a467d0108d37a22d8f5b7b21dd02c4da
Nathalia Duarte <duartenatha@gmail.com>
2022-04-22 10:07:32 -0300

    testing arrow positioning

--- src/pages/addticket/AddTicket.tsx
+++ src/pages/addticket/AddTicket.tsx
@@ -6,13 +6,24 @@ import {
 } from 'formik';
 import { TicketDTO } from '../../models/TicketDTO';
 import * as Yup from 'yup';
-import { StyledForm, ButtonDefault, ContainerMain, StyledLabel, InputDefault } from '../../global.styles'
+import { StyledForm, ButtonDefault, ContainerMain, StyledLabel, InputDefault, DivFlexColumn, DivFlex, PageTitle, LinkBack, } from '../../global.styles'
+import { ContainerAddTicket, DivFlexItem, AnotherItem } from './AddTicket.style';
+import { DivButton } from '../signup/SignUp.style';
+import {
+  AiOutlineArrowLeft,
+} from 'react-icons/ai';
 
 const AddTicket = () => {
   const addTicketSchema = Yup.object().shape({
     title: Yup.string().required('Campo obrigatório.'),
     date: Yup.string().required('Campo obrigatório.'),
     totalSum: Yup.string().required('Campo obrigatório.'),
+    items: Yup.array().of(Yup.object().shape({
+      sum: Yup.string().required('Campo obrigatório.'),
+      nameItem: Yup.string().required('Campo obrigatório.'),
+      dateItem: Yup.string().required('Campo obrigatório.'),
+      attachment: Yup.string().required('Campo obrigatório.'),
+    })).min(1, "Informe ao menos um item.")
   });
   const formik = useFormik({
     initialValues: {
@@ -43,73 +54,95 @@ const AddTicket = () => {
   });
   return (
     <ContainerMain>
+      <LinkBack to="/"><AiOutlineArrowLeft /></LinkBack>
+      <ContainerAddTicket>
+      <PageTitle>Pedido de reembolso</PageTitle>
       <StyledForm onSubmit={formik.handleSubmit}>
+
+      <DivFlexColumn>
         <StyledLabel htmlFor="date">Título:</StyledLabel>
         <InputDefault
           id="title"
           name="title"
-          placeholder="Digite seu título completo"
+          placeholder="Digite o título"
           value={formik.values.title}
           onChange={formik.handleChange}
         />
+        </DivFlexColumn>
 
+        <DivFlexColumn>
         <StyledLabel htmlFor="date">Data:</StyledLabel>
         <InputDefault
           id="date"
           name="date"
-          placeholder="Digite seu título completo"
+          placeholder="Digite a data"
           value={formik.values.date}
           onChange={formik.handleChange}
         />
+        </DivFlexColumn>
 
+        <DivFlexColumn>
         <StyledLabel htmlFor="totalSum">Total:</StyledLabel>
         <InputDefault
           id="totalSum"
           name="totalSum"
-          placeholder="Digite seu título completo"
+          placeholder="Digite o valor completo"
           value={formik.values.totalSum}
           onChange={formik.handleChange}
         />
+        </DivFlexColumn>
 
+        
         <FormikProvider value={formik}>
+        
           <FieldArray
             name="items"
             render={(ArrayHelpers) => (
               <div>
                 {formik.values.items.map((item, index) => (
-                  <div key={index}>
+                  <DivFlexItem key={index}>
+                    <StyledLabel htmlFor="item">Dados do pedido de reembolso:</StyledLabel>
                     <InputDefault
                       name={`items[${index}.nameItem]`}
                       id={`items[${index}.nameItem]`}
                       value={item.nameItem}
                       onChange={formik.handleChange}
+                      placeholder="Item:"
                     />
+                    <DivFlex>
                     <InputDefault
                       name={`items[${index}.dateItem]`}
                       id={`items[${index}.dateItem]`}
                       value={item.dateItem}
                       onChange={formik.handleChange}
+                      placeholder="Data:"
                     />
                     <InputDefault
                       name={`items[${index}.sum]`}
                       value={item.sum}
                       onChange={formik.handleChange}
+                      placeholder="Valor:"
                     />
+                    </DivFlex>
                     <InputDefault
                       name={`items[${index}.attachment]`}
                       value={item.attachment}
                       onChange={formik.handleChange}
+                      type="file"
                     />
-                    <button
+                    <DivButton>
+                    <ButtonDefault
                       type="button"
                       onClick={() => ArrayHelpers.remove(index)}
                     >
                       Remover
-                    </button>
-                  </div>
+                    </ButtonDefault>
+                    </DivButton>
+                  </DivFlexItem>
                 ))}
-                <button
-                  type="button"
+                <DivButton>
+                <AnotherItem
+                href='#'
                   onClick={() =>
                     ArrayHelpers.push({
                       nameItem: '',
@@ -119,14 +152,18 @@ const AddTicket = () => {
                     })
                   }
                 >
-                  Adicionar outro item
-                </button>
+                  Deseja adicionar outro item?
+                </AnotherItem>
+                </DivButton>
               </div>
             )}
           ></FieldArray>
         </FormikProvider>
+        <DivButton>
         <ButtonDefault type="submit">Enviar</ButtonDefault>
+        </DivButton>
       </StyledForm>
+      </ContainerAddTicket>
     </ContainerMain>
   );
 };
