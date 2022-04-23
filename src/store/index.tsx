92a5e6b09d06a5f9969064507390cf63932ed4a6
Nathalia Duarte <duartenatha@gmail.com>
2022-04-23 12:35:12 -0300

    redux persist added

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,5 +1,23 @@
 import { legacy_createStore, compose } from 'redux';
 import rootReducer from './reducers';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
+import { AuthDTO } from '../models/AuthDTO';
+import { WebStorage } from 'redux-persist/lib/types';
+
+
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+};
+
+interface Ipersist {
+  key: string,
+  storage: object,
+}
+
+const pReducer = persistReducer(persistConfig, rootReducer);
 
 //#region Redux DevTools
 declare global {
@@ -10,9 +28,9 @@ declare global {
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 //#endregion Redux DevTools
 
-const store = legacy_createStore(rootReducer, composeEnhancers());
+export const store = legacy_createStore(pReducer, composeEnhancers());
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
-export default store;
+export const persistor = persistStore(store)
