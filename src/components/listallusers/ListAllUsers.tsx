e729eafa6e9f0543d9e867b6020e83b992f021b1
Nathalia Duarte <duartenatha@gmail.com>
2022-04-24 13:03:58 -0300

    list map working

--- src/components/listallusers/ListAllUsers.tsx
+++ src/components/listallusers/ListAllUsers.tsx
@@ -1,23 +1,36 @@
 import { listAllUsers } from '../../store/actions/ListUsersActions';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { RootState } from '../../store';
 import { AnyAction } from 'redux';
 
 const ListAllUsers = (state:RootState & AnyAction) => {
   console.log(state, "state")
-  const {dispatch, users} = state
+  const {dispatch, users, loading} = state
+  const [hasList, setHasList] = useState(false)
+  
   
   useEffect(() => {
+    if (users.length === 1){
     listAllUsers(users, dispatch);
-  }, []);
+    }
+    // if (users.length > 1){
+    //   setHasList(true)
+    //   setLoading(false)
+    //   console.log('entrou')
+    // }
+  }, [users]);
 
-  
+
+  if(loading){
+    return (<div>Loading</div>)
+  }
+
   console.log(users, "user")
   return (
     <div><h1>Lista de usuários</h1>
     {users.map((user:any)=>(
-      <p key={user.idUser}>{user.name}</p>
+      <div key={user.idUser}><p>{user.name}</p></div>
     ))}
     <h2>oi</h2>
     </div>
@@ -26,7 +39,7 @@ const ListAllUsers = (state:RootState & AnyAction) => {
 
 const mapStateToProps = (state: RootState) => ({
   users: state.list.users,
-  
+  loading: state.list.loading,
 });
 
 export default connect(mapStateToProps)(ListAllUsers);
--- src/store/actions/ListUsersActions.tsx
+++ src/store/actions/ListUsersActions.tsx
@@ -7,7 +7,7 @@ export const listAllUsers = async ( users: ListUsersDTO, dispatch: AppDispatch )
     console.log(users, "user dentro da action")
     try {
         const {data} = await api.get('/user/listAllUser')
-        const list = {users: [data], type: 'LIST_USERS'}
+        const list = {users: data, type: 'LIST_USERS'}
         dispatch (list)
     } catch (error) {
         console.log(error)
