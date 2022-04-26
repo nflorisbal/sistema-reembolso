680fcb91172c478d630fed3d55de9ec07679b137
Nathalia Duarte <duartenatha@gmail.com>
2022-04-26 16:24:38 -0300

    changes made to add ticket validation, not workring as of now

--- src/models/TicketDTO.tsx
+++ src/models/TicketDTO.tsx
@@ -2,11 +2,19 @@ export interface TicketDTO {
   title: string;
   totalValue?: string;
   situation?: string;
-  items: 
-    {
-      name: string;
-      dateItem: string;
-      value: string;
-      image: string;
-    }[]
+  items: {
+    name:string;
+    image: string;
+    dateItem: string;
+    value: string;
+  }[]
 }
+
+// export interface ITicketItems{
+//   {
+//     name:string;
+//     image: string;
+//     dateItem: string;
+//     value: string;
+//   }[]
+// }
