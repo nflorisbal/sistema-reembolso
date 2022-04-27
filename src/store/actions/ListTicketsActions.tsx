607b0d036cb92af35db07aa937f6910c7fb7957e
Nathalia Duarte <duartenatha@gmail.com>
2022-04-27 10:16:58 -0300

    added list tickets to the store

---
+++ src/store/actions/ListTicketsActions.tsx
@@ -0,0 +1,23 @@
+import { AppDispatch } from '..';
+import api from '../../api';
+
+export const listTickets = async (dispatch: AppDispatch, token: any) => {
+  try {
+    const { data } = await api.get(
+      '/refund/?page=1&size=10',
+      (api.defaults.headers.common['Authorization'] = token)
+    );
+
+    const tickets = {
+      type: 'LIST_TICKETS',
+      tickets: data.content,
+      totalPages: data.totalPages,
+      totalElements: data.totalElements,
+      loading: false,
+    };
+
+    dispatch(tickets);
+  } catch (error) {
+    console.log(error);
+  }
+};
