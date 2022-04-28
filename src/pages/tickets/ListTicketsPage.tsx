313abdfcafeb04cd5a1e52e7b8709a02085629b1
Nathalia Duarte <duartenatha@gmail.com>
2022-04-28 11:27:18 -0300

    added treatment for when there arent any tickets

--- src/pages/tickets/ListTicketsPage.tsx
+++ src/pages/tickets/ListTicketsPage.tsx
@@ -1,23 +1,40 @@
 import { useEffect } from 'react';
+import { connect, DispatchProp } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import ListTickets from '../../components/listtickets/ListTickets';
+import ZeroTicket from '../../components/zeroticket/ZeroTicket';
+import { ArrayTicketDTO } from '../../models/TicketDTO';
+import { RootState } from '../../store';
 import { hasToken } from '../../utils';
 
-const ListTicketsPage = () => {
+const ListTicketsPage = (state: ArrayTicketDTO & DispatchProp) => {
+
+  const {ticketsList} = state;
+  console.log(state)
   const navigate = useNavigate();
 
   useEffect(() => {
     if (!hasToken()) {
       navigate('/login');
     }
+
     // eslint-disable-next-line
   }, []);
 
   return (
-    <>
-      <ListTickets />
+    <>{ticketsList.length  
+      ? <ListTickets />
+      : <ZeroTicket />
+      }
     </>
   );
 };
 
-export default ListTicketsPage;
+const mapStateToProps = (state: RootState) => ({
+  ticketsList: state.tickets.ticketsList,
+  loadingTickets: state.tickets.loadingTickets,
+  totalElements: state.tickets.totalElements,
+  totalPages: state.tickets.totalPages,
+});
+
+export default connect(mapStateToProps)(ListTicketsPage);
